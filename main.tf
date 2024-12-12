4a335650f8a4f0c7fe9bcb54f2c8b2ca6e245c18
joelovegit <joe.love@jetdillo.com>
2024-12-12 14:47:38 -0800

    add public ip association

--- main.tf
+++ main.tf
@@ -36,6 +36,7 @@ resource "aws_instance" "blog" {
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   subnet_id = module.blog_vpc.public_subnets[0]
+  associate_public_ip_address = true
 
   tags = {
     Name = "HelloWorld"
