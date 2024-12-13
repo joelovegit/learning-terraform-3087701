3cff50a96d4dfb9efd9d91738f42d1c09b6a857a
joelovegit <joe.love@jetdillo.com>
2024-12-13 12:12:56 -0800

    rollback a bit

--- main.tf
+++ main.tf
@@ -29,48 +29,17 @@ module "blog_vpc" {
   }
 }
 
-module "blog_alb" {
-  source = "terraform-aws-modules/alb/aws"
+resource "aws_instance" "blog" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 
-  name    = "blog-alb"
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
-  security_groups = [ module.blog_sg.security_group_id ]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  listeners = {
-    ex-http-https-redirect = {
-      port     = 80
-      protocol = "HTTP"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
-    }
-    ex-https = {
-      port            = 443
-      protocol        = "HTTPS"
-      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-
-      forward = {
-        target_group_key = "ex-instance"
-      }
-    }
-  }
-
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "blog"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      target_id        = aws_instance.blog.id
-    }
-  }
+  subnet_id = module.blog_vpc.public_subnets[0]
+  associate_public_ip_address = true
 
   tags = {
-    Environment = "dev"
-    Project     = "Example"
+    Name = "HelloWorld"
   }
 }
 
